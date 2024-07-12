36d3870ed0abf6a385beef7b4c4978bc13e0b850
BhanuPrakash <99791736+Bhanuprakash93@users.noreply.github.com>
2024-07-12 19:30:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -202,11 +202,11 @@ x_col1 = st.selectbox('Select first column for grid', feature_columns_NLM)
 x_col2 = st.selectbox('Select second column for grid', feature_columns_NLM)
 
 st.header('Input Values for Grid')
-pmin = st.number_input(f'Enter minimum {x_col1} value', min_value=None, max_value=None, step=0.005)
-pmax = st.number_input(f'Enter maximum {x_col1} value', min_value=None, max_value=None, step=0.005)
-dmMin = st.number_input(f'Enter minimum {x_col2} value', min_value=None, max_value=None, step=0.005)
-dmMax = st.number_input(f'Enter maximum {x_col2} value', min_value=None, max_value=None, step=0.005)
-step_size = st.number_input('Enter step size', min_value=None, max_value=None, step=0.005)
+pmin = st.number_input(f'Enter minimum {x_col1} value', min_value=0.0, max_value=None, step=0.005)
+pmax = st.number_input(f'Enter maximum {x_col1} value', min_value=0.0, max_value=None, step=0.005)
+dmMin = st.number_input(f'Enter minimum {x_col2} value', min_value=0.0, max_value=None, step=0.005)
+dmMax = st.number_input(f'Enter maximum {x_col2} value', min_value=0.0, max_value=None, step=0.005)
+step_size = st.number_input('Enter step size', min_value=0.0, max_value=None, step=0.005)
 
 # Selection for X-axis column in 2D plot
 x_axis_column = st.selectbox('Select the column to use for the X-axis in the 2D plot', [x_col1, x_col2])
